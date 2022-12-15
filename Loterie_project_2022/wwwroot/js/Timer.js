ee3be676f657f6981b23cf2a2e36003d96c479ae
anna <anna.logina@ccicampus.fr>
2022-12-15 11:07:06 +0100

    commit js

--- Loterie_project_2022/wwwroot/js/Timer.js
+++ Loterie_project_2022/wwwroot/js/Timer.js
@@ -5,58 +5,26 @@ var jsDateEnd = new Date(Date.parse(timeend));
 
 
 
+    setInterval(function () {
 
-setInterval(function () {
+        const total = Date.parse(jsDateEnd) - Date.parse(new Date());
+        const seconds = Math.floor((total / 1000) % 60);
+        const minutes = Math.floor((total / 1000 / 60) % 60);
 
-    var d = new Date();
+        if (minutes < 1) {
+            document.getElementById('play').setAttribute("disabled", "disabled");
+        }
+        
 
-    if (d >= jsDate) {
+        document.getElementById('timer').innerHTML = minutes+":"+seconds;
+        // };
 
-    console.log(d, jsDate);
-   
-    var seconds = d.getMinutes() * 60 + d.getSeconds();
-    console.log(seconds);
-    var fiveMin = 60 * 5;
-    var timeleft = fiveMin - seconds % fiveMin;
-        var result = parseInt(timeleft / 60) + ':' + timeleft % 60;
-    }
+        if (minutes == 0) {
+            clearInterval(countdownInterval);
+            window.location.reload();
+        }
 
-     document.getElementById('timer').innerHTML = result;
-   // };
-
-    if (d === jsDateEnd) {
-        clearInterval(countdownInterval);
-    }
-
-}, 500);
-
-
-/*
-
-// Set the date we're counting down to
-var time = document.getElementById("timeref").value;
-var jsDate = new Date(Date.parse(time));
-
-var timeend = document.getElementById("timerefend").value;
-var jsDateEnd = new Date(Date.parse(timeend));
+    }, 500);
 
 
 
-var distance = jsDateEnd.getTime() - jsDate.getTime();
-distance = distance / 1000;
-
-const timer = document.getElementById('timer');
-
-// Update the count down every 1 second
-setInterval(function () {
-
-    var distance = jsDateEnd.getTime() - jsDate.getTime();
-    distance = distance / 1000;
-   
-    var minutes = Math.floor(distance / 60);
-    var seconds = Math.floor(distance) % 60;
-   
-    timer.innerHTML = `${minutes}:${seconds}`;
-
-    
-}, 500);*/
